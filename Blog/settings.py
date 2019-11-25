d5e4cf39fdc0fcc5bfa6d0e359a653d0221efa73
Nishant <inishant7@outlook.com>
2019-11-25 12:07:55 +0530

    final ML

--- Blog/settings.py
+++ Blog/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
+    'Blog.apps.BlogConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
--- blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Result, File
-
-# Register your models here.
-admin.site.register(Result)
-admin.site.register(File)
